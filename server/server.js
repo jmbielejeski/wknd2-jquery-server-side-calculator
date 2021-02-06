4d8d2f9830781c9ca1b14150872165bbcebfc439
jmbielejeski <jmbielejeski@gmail.com>
2021-02-06 10:43:22 -0700

    setup basic files, server and public

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+// const bodyParser = require('body-parser');
+
+// Create our app
+const app = express();
+const port = 5000;
+
+// INCANTATION:
+// Share any files inside the "./server/public" folder
+app.use(express.static('server/public'));
+
+// **** OPTIONAL BODY PARSER USAGE
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Listen for network requests
+app.listen(port, function () {
+  // When the server is ready, call this function
+  console.log(`I'm listening.... `, port);
+});
