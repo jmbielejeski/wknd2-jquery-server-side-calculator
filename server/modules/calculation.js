7e27d5d25affe664b1d1b5f3743ee1fba5806741
jmbielejeski <jmbielejeski@gmail.com>
2021-02-06 10:56:13 -0700

    fleshing out the server

---
+++ server/modules/calculation.js
@@ -0,0 +1,7 @@
+const { builtinModules } = require("module");
+
+function calculate(inputOne, inputTwo) {
+  console.log('performing calculation on ', inputOne, inputTwo);
+}
+
+modules exports = calculate;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-// const bodyParser = require('body-parser');
+const calculate = require('./modules/calculate');
 
 // Create our app
 const app = express();
@@ -11,8 +12,8 @@ const port = 5000;
 app.use(express.static('server/public'));
 
 // **** OPTIONAL BODY PARSER USAGE
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,3 +23,6 @@ app.listen(port, function () {
   // When the server is ready, call this function
   console.log(`I'm listening.... `, port);
 });
+
+// get history to send to client.
+// history array of objects
